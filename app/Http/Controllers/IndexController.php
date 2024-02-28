cbc1ec6c17933fc844c2d094176af2670f6fca0b
arrput <ari21putra@gmail.com>
2024-02-28 14:51:20 +0700

    rev manage user

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -14,7 +15,8 @@ class IndexController extends Controller
         return view('admin.index', compact('departments'));
     }
     public function manageUser(){
+        $data = User::orderBy('id','DESC')->get();
         
-        return view('admin.manage_user.index');
+        return view('admin.user.index',compact('data'));
     }
 }
